0132b69052860368a6c8790c6e57548a400fd2e4
David Higgins <thehig@gmail.com>
2018-11-21 13:18:20 +0000

    Add env port for Heroku

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 1337;
+const port = process.env.PORT || 8080;
 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/index.html"));
