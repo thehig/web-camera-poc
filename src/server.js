233d88cd65a941ca758c90953ceedd708c2645ed
David Higgins <thehig@gmail.com>
2018-11-21 14:43:05 +0000

    Add body parser & post image src to express console

--- src/server.js
+++ src/server.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const bodyParser = require('body-parser')
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(bodyParser.json());
+
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.post("/api/images", (req, res) => {
+  console.log(req.body);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
